6b9792a7dcb19a5acd91373bf2014a910777b76d
Ayush <ayush08.pujari@gmail.com>
2022-08-04 16:12:31 +0530

    updated cease cipher!!!

--- Caeser & Cipher/Main.py
+++ Caeser & Cipher/Main.py
@@ -2,54 +2,45 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt and 'decode to decrypt.\n")
-text = input("Type your message here:\n").lower()
-shift = int(input("Type the shift number:\n"))
+condition = "yes"
 
 
-# Function for Encoding the words given as input
-def encrypt(plain_text, shift_amount=0):
+# Function for Encoding and decoding the words given as input
+def caesar(start_text, shift_amount, cipher_direction):
     """
-    Inside this function, shift each letter of the 'text' forwards in the alphabetical order by shifting the shift
-    amount and print the encrypted text.
-
-    :param plain_text:
+    This will allow the user to encode or decode the code with the desired direction provided.
+    :param start_text:
     :param shift_amount:
-    :return: str:
+    :param cipher_direction:
+    :return:
     """
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+    end_text = ""
 
-    print(f"The Encoded Text is {cipher_text}")
-    # return cipher_text
+    if cipher_direction == 'decode':
+        shift_amount *= -1
 
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
 
-def decrypt(cipher_text, shift_amount=0):
-    """
-        Inside this function, shift each letter of the 'text' backward in the alphabetical order by shifting the shift
-        amount and print the decrypted text.
-
-        :param cipher_text:
-        :param shift_amount:
-        :return: str:
-        """
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-
-    print(f"The Decoded text is {plain_text}")
-    # return plain_text
+    print(f"The {cipher_direction}d text is {end_text}")
 
 
-if direction == 'encode':
-    encrypt(plain_text=text, shift_amount=shift)
+while condition == 'yes':
 
-elif direction == 'decode':
-    decrypt(cipher_text=text, shift_amount=shift)
+    direction = input("Type 'encode' to encrypt and 'decode to decrypt.\n")
+    text = input("Type your message here:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    condition = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if condition == 'no':
+        print("Goodbye")
+        break
+    else:
+        continue
